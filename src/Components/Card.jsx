e1cdbf20c214f08f75b4ba1d5ae60b7ab99544cb
Zura Doghanadze <zurabi.doghanadze.1@iliauni.edu.ge>
2023-10-24 11:17:02 +0400

    add proptypes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,63 @@
 import styled from "styled-components";
 import Lines from "../assets/img/Line.png";
+import PropTypes from "prop-types";
 
+function Card(props) {
+  Card.propTypes = {
+    names: PropTypes.string.isRequired,
+    cardNumber: PropTypes.string.isRequired,
+    mm: PropTypes.string.isRequired,
+    yy: PropTypes.string.isRequired,
+    eg: PropTypes.string.isRequired,
+  };
+  return (
+    <CardContainer>
+      <FrontSide>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="84"
+            height="47"
+            viewBox="0 0 84 47"
+            fill="none"
+          >
+            <ellipse
+              cx="23.4783"
+              cy="23.5"
+              rx="23.4783"
+              ry="23.5"
+              fill="white"
+            />
+            <path
+              d="M83.5 23.5C83.5 29.0647 78.9932 33.575 73.4348 33.575C67.8764 33.575 63.3696 29.0647 63.3696 23.5C63.3696 17.9353 67.8764 13.425 73.4348 13.425C78.9932 13.425 83.5 17.9353 83.5 23.5Z"
+              stroke="white"
+            />
+          </svg>{" "}
+        </div>
+        <CardNumberContainer>
+          {props.cardNumber ? props.cardNumber : "0000 0000 0000 0000"}
+        </CardNumberContainer>
+        <NameWrapper>
+          <div>{props.names ? props.names : "JANE APPLESEED"}</div>
+          <div>
+            {props.mm ? props.mm : "00"} / {props.yy ? props.yy : "00"}
+          </div>
+        </NameWrapper>
+      </FrontSide>
+      <BackSideWrap>
+        <BackSide>
+          <BlackLine></BlackLine>
+          <CVCContainer>{props.eg ? props.eg : "000"}</CVCContainer>
+          <div>
+            <img src={Lines} alt="" />
+          </div>
+        </BackSide>
+      </BackSideWrap>
+    </CardContainer>
+  );
+}
+
+export default Card;
 const CardContainer = styled.div`
   margin: 0 30px;
   max-width: 533px;
@@ -87,53 +144,3 @@ const CVCContainer = styled.div`
   justify-content: flex-end;
   align-items: center;
 `;
-function Card(props) {
-  return (
-    <CardContainer>
-      <FrontSide>
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="84"
-            height="47"
-            viewBox="0 0 84 47"
-            fill="none"
-          >
-            <ellipse
-              cx="23.4783"
-              cy="23.5"
-              rx="23.4783"
-              ry="23.5"
-              fill="white"
-            />
-            <path
-              d="M83.5 23.5C83.5 29.0647 78.9932 33.575 73.4348 33.575C67.8764 33.575 63.3696 29.0647 63.3696 23.5C63.3696 17.9353 67.8764 13.425 73.4348 13.425C78.9932 13.425 83.5 17.9353 83.5 23.5Z"
-              stroke="white"
-            />
-          </svg>{" "}
-        </div>
-        <CardNumberContainer>
-          {props.cardNumber ? props.cardNumber : "0000 0000 0000 0000"}
-        </CardNumberContainer>
-        <NameWrapper>
-          <div>{props.names ? props.names : "JANE APPLESEED"}</div>
-          <div>
-            {/* {props.mm ? props.mm : "00" + "/" + props.yy ? props.yy : "00"} */}
-            {props.mm ? props.mm : "00"} / {props.yy ? props.yy : "00"}
-          </div>
-        </NameWrapper>
-      </FrontSide>
-      <BackSideWrap>
-        <BackSide>
-          <BlackLine></BlackLine>
-          <CVCContainer>{props.eg ? props.eg : "000"}</CVCContainer>
-          <div>
-            <img src={Lines} alt="" />
-          </div>
-        </BackSide>
-      </BackSideWrap>
-    </CardContainer>
-  );
-}
-
-export default Card;
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,7 +2,111 @@ import styled from "styled-components";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { useState } from "react";
+import PropTypes from "prop-types";
+
+const schema = yup
+  .object({
+    cardholderName: yup.string().required(),
+    cardName: yup.string().required().length(16),
+    age: yup.number().positive().integer().required(),
+    mm: yup.number().positive().integer().min(2).required(),
+    yy: yup.number().positive().integer().min(2).required(),
+    cvc: yup.number().positive().integer().min(3).required(),
+  })
+  .required();
+
+function Form(props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) => console.log(data);
+  Form.propTypes = {
+    names: PropTypes.string.isRequired,
+    cardNumber: PropTypes.string.isRequired,
+    mm: PropTypes.string.isRequired,
+    yy: PropTypes.string.isRequired,
+    eg: PropTypes.string.isRequired,
+    setNames: PropTypes.func.isRequired,
+    setCardNUmber: PropTypes.func.isRequired,
+    setMm: PropTypes.func.isRequired,
+    setYy: PropTypes.func.isRequired,
+    setEg: PropTypes.func.isRequired,
+  };
+  return (
+    <FormWrapper>
+      <div>
+        <FormContainer onSubmit={handleSubmit(onSubmit)}>
+          <NameNumberWrap>
+            <Label htmlFor="">Cardholder name</Label>
+            <NameInput
+              {...register("cardholderName")}
+              value={props.names}
+              onChange={(event) => props.setNames(event.target.value)}
+              type="text"
+              placeholder="e.g. Jane Appleseed"
+            />
+            {errors.cardholderName && (
+              <ErrorMessage>This field is empty</ErrorMessage>
+            )}
+            <Label htmlFor="">Card Number</Label>
+            <CardNumberInput
+              {...register("cardName")}
+              value={props.cardNumber}
+              onChange={(event) => props.setCardNUmber(event.target.value)}
+              type="text"
+              placeholder="e.g. 1234 5678 9123 0000"
+            />
+            {errors.cardName && (
+              <ErrorMessage>Wrong format, numbers only</ErrorMessage>
+            )}
+          </NameNumberWrap>
+          <DataWrapper>
+            <DataContainer>
+              <Label htmlFor="">Exp. Date (MM/YY)</Label>
+              <div>
+                <MMInput
+                  {...register("mm")}
+                  value={props.mm}
+                  onChange={(event) => props.setMm(event.target.value)}
+                  type="text"
+                  placeholder="MM"
+                />
+                <YYInput
+                  {...register("yy")}
+                  value={props.yy}
+                  onChange={(event) => props.setYy(event.target.value)}
+                  type="text"
+                  placeholder="YY"
+                />
+              </div>
+              {(errors.mm || errors.yy) && (
+                <ErrorMessage>Can’t be blank</ErrorMessage>
+              )}
+            </DataContainer>
+            <div>
+              <Label htmlFor="">CVC</Label>
+              <CCVInput
+                {...register("cvc")}
+                value={props.eg}
+                onChange={(event) => props.setEg(event.target.value)}
+                type="text"
+                placeholder="e.g. 123"
+              />
+              {errors.cvc && <ErrorMessage>Can’t be blank</ErrorMessage>}
+            </div>
+          </DataWrapper>
+          <Button type="submit">Confirm</Button>
+        </FormContainer>
+      </div>
+    </FormWrapper>
+  );
+}
+export default Form;
+
 
 const FormWrapper = styled.div`
   max-width: 380px;
@@ -120,95 +224,3 @@ const ErrorMessage = styled.p`
   font-size: 14px;
   margin-top: 8px;
 `;
-
-const schema = yup
-  .object({
-    cardholderName: yup.string().required(),
-    cardName: yup.string().required().length(16),
-    age: yup.number().positive().integer().required(),
-    mm: yup.number().positive().integer().min(2).required(),
-    yy: yup.number().positive().integer().min(2).required(),
-    cvc: yup.number().positive().integer().min(3).required(),
-  })
-  .required();
-
-function Form(props) {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
-  return (
-    <FormWrapper>
-      <div>
-        <FormContainer onSubmit={handleSubmit(onSubmit)}>
-          <NameNumberWrap>
-            <Label htmlFor="">Cardholder name</Label>
-            <NameInput
-              {...register("cardholderName")}
-              value={props.names}
-              onChange={(event) => props.setNames(event.target.value)}
-              type="text"
-              placeholder="e.g. Jane Appleseed"
-            />
-            {errors.cardholderName && (
-              <ErrorMessage>This field is empty</ErrorMessage>
-            )}
-            <Label htmlFor="">Card Number</Label>
-            <CardNumberInput
-              {...register("cardName")}
-              value={props.cardNumber}
-              onChange={(event) => props.setCardNUmber(event.target.value)}
-              type="text"
-              placeholder="e.g. 1234 5678 9123 0000"
-            />
-            {errors.cardName && (
-              <ErrorMessage>Wrong format, numbers only</ErrorMessage>
-            )}
-          </NameNumberWrap>
-          <DataWrapper>
-            <DataContainer>
-              <Label htmlFor="">Exp. Date (MM/YY)</Label>
-              <div>
-                <MMInput
-                  {...register("mm")}
-                  value={props.mm}
-                  onChange={(event) => props.setMm(event.target.value)}
-                  type="text"
-                  placeholder="MM"
-                />
-                <YYInput
-                  {...register("yy")}
-                  value={props.yy}
-                  onChange={(event) => props.setYy(event.target.value)}
-                  type="text"
-                  placeholder="YY"
-                />
-              </div>
-              {(errors.mm || errors.yy) && (
-                <ErrorMessage>Can’t be blank</ErrorMessage>
-              )}
-            </DataContainer>
-            <div>
-              <Label htmlFor="">CVC</Label>
-              <CCVInput
-                {...register("cvc")}
-                value={props.eg}
-                onChange={(event) => props.setEg(event.target.value)}
-                type="text"
-                placeholder="e.g. 123"
-              />
-              {errors.cvc && <ErrorMessage>Can’t be blank</ErrorMessage>}
-            </div>
-          </DataWrapper>
-          <Button type="submit">Confirm</Button>
-        </FormContainer>
-      </div>
-    </FormWrapper>
-  );
-}
-export default Form;
